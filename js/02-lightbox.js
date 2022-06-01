c1b2d75f571740bf87e88ed3fa3c9f03a5f48b96
Anatolii <anatoliirst@gmail.com>
2022-06-01 18:14:41 +0200

    end HW7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+    const addHtml = galleryItems.map((item) =>
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" 
+    alt="${item.description}"/>
+    </a>`
+).join("");
+
+galleryRef.insertAdjacentHTML('beforeend', addHtml)
+
+function onClick (e) {
+    e.preventDefault();
+    let modal = new SimpleLightbox('.gallery__item', {captionsData:'alt', captionDelay:250});
+    modal.on('show.SimpleLightbox');
+}
+
+galleryRef.addEventListener('click', onClick);
